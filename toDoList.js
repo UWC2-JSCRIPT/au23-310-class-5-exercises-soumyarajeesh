2d9d2ac59d9a5707b532fcd5520659e3fb3f8fa4
Soumya Rajeesh <soumyatk@uw.edu>
2023-11-13 13:46:04 -0800

    StoppingBehavior

--- toDoList.js
+++ toDoList.js
@@ -5,10 +5,53 @@
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
-const addListItem = function(e) {
-  e.preventDefault();
-  const input = this.parentNode.getElementsByTagName('input')[0];
-  const text = input.value; // use this text to create a new <li>
+//const addListItem = function(e) {
+  //e.preventDefault();
+  //const input = this.parentNode.getElementsByTagName('input')[0];
+  //const text = input.value; // use this text to create a new <li>
 
   // Finish function here
-};
+//};
+// Get references to elements
+const todayList = document.querySelector('.today-list');
+const addItemInput = document.querySelector('.add input');
+const addItemButton = document.querySelector('.add-item');
+
+// Function to create a new list item
+function createListItem(text) {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `<span>${text}</span> <a class="delete">Delete</a>`;
+  todayList.appendChild(listItem);
+
+  // Add click event listener to toggle "done" class
+  listItem.addEventListener('click', function () {
+    listItem.classList.toggle('done');
+  });
+
+  // Add click event listener to delete the item
+  const deleteLink = listItem.querySelector('.delete');
+  deleteLink.addEventListener('click', function () {
+    todayList.removeChild(listItem);
+  });
+
+  // Clear the input field after adding an item
+  addItemInput.value = '';
+}
+
+// Event listener to add a new item
+addItemButton.addEventListener('click', function () {
+  const text = addItemInput.value.trim();
+  if (text) {
+    createListItem(text);
+  }
+});
+
+// Event listener to add a new item when Enter key is pressed
+addItemInput.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    const text = addItemInput.value.trim();
+    if (text) {
+      createListItem(text);
+    }
+  }
+});
--- traversing.js
+++ traversing.js
@@ -1,11 +1,20 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
-const body = document.querySelector('body');
+//const body = document.querySelector('body');
+const body = document.body; 
+const mainElement = body.querySelector("main"); // Access the <main> node
+console.log(mainElement);
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
-const ul = document.querySelector('ul');
+//const ul = document.querySelector('ul');
+const ul = document.querySelector("ul"); // Get the <ul> element
+const bodyElement = ul.closest("body"); // Access the <body> element using closest
+console.log(bodyElement);
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
-const p = document.querySelector('p');
+//const p= document.querySelector('p');
+const p1= document.querySelector("p"); // Get the <p> element
+const thirdLiInP = p1.querySelectorAll("li")[2]; // Access the 3rd <li> in the <p> using querySelectorAll
+console.log(thirdLiInP);
